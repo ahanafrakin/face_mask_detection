a80e0e05ff7cc15e590479c817990ba27afeb840
Erebus <erebus_illyrium@github.com>
2021-01-16 20:07:41 -0500

    Continously loop capturing frame by frame detecting faces

--- opencv_face_detection_algorithm/main.py
+++ opencv_face_detection_algorithm/main.py
@@ -17,3 +17,28 @@ face_cascades = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Get a live video from the webcam, face detection improves with better lighting so ensure the room is well lit
 # possible disadvantage is capturing video at night since reduced lighting reduces accuracy
 live_video = cv2.VideoCapture(0)
+
+# Continuously loop reading frames until I press an escape key
+while True:
+    # Get the frames from live video, return code tells us if we're done scanning frames but with webcam
+    # we won't run out
+    return_code, frame = live_video.read()
+
+    # OpenCV uses grayscale so we convert the frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect the faces in the cascade, returns a list of rectangles where faces are supposedly found
+    faces = face_cascades.detectMultiScale(
+        gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+
+    # Draw a rectangle around a face, (x,y) top left, w : width, h : height
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # Display the frame
+    cv2.imshow('Video', frame)
+
+    # quit if keyboard interrupt...wait for 'q' to be pressed
+    k = cv2.waitKey(1) & 0xff == ord('q')
+    if k == 27:
+        break
