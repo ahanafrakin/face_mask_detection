363e3e893220353704f45c367e9a59e4e8c12192
Erebus <erebus_illyrium@github.com>
2021-01-16 20:05:13 -0500

    created main file for face detection using open CV 2

---
+++ opencv_face_detection_algorithm/main.py
@@ -0,0 +1,12 @@
+# using open CV 2 for face detection
+import cv2
+
+'''
+    The method of face detection used will be haar cascade classifiers
+    This is an ML approach that uses a cascade function that is trained from positive and 
+    negative images to detect objects in images. 
+    
+    The xml file saved in the this working directory contains all the data needed to train open cv Cascade 
+    Classifier
+    
+'''
